78a3e86d212aa8271b99eedc3fdce05ba5ac2775
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-14 09:55:48 +0100

    Add example db

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
+const db = [
+  {id: 1, author: 'John Doe', text: 'This company is worth every coin!'},
+  {id: 2, author: 'Amanda Doe', text: 'The really know how to make you happy'},
+];
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
