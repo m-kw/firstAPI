5c98bc655ff53edd258868347133c6c99999b6f9
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-14 14:27:35 +0100

    Prepare app to be published on Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const uuidv4 = require('uuid/v4');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -12,16 +13,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname + '/client/build')));
+
 app.use(cors());
 
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000');
 });
