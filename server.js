cd1119e2589e7c650618c5dc313470d995f99e20
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-28 11:08:01 +0100

    Add models for concert, seat, testimonial and add controller functions for concerts.routes

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
 
-const testimonialsRoutes = require('./routes/testimonials.routes');
+// const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
+// const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/', testimonialsRoutes);
+// app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
-app.use('/api/', seatsRoutes);
+// app.use('/api/', seatsRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
